e29b2c2eb25a1fafdc91489debf62a100ef36fc8
Clay Sawyer <claysawyer@outlook.com>
2023-05-11 13:35:28 -0500

    add Link component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
-function App() {}
+import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordianPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+
+function App() {
+  return (
+    <div>
+      <Link to="/accordian">Go to accordian</Link>
+      <Link to="/dropdown">Go to dropdown</Link>
+      <div>
+        <Route path="/accordian">
+          <AccordianPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+      </div>
+    </div>
+  );
+}
 
 export default App;
